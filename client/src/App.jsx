7a9cb09412a5e29a482ab99a3395f4a646c37596
imcarlosmsX <sandovalcarlos@uninorte.edu.co>
2024-05-23 03:08:58 -0500

    react

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,17 @@
-import {BrowserRouter, Routes, Route} from "react-roputer-dom";
+import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
 import {Register} from './pages/register';
+import {Login} from './pages/login';
+import {Navigation} from './components/navigation';
 
 function App() {
   return (
+
     <BrowserRouter>
+      <Navigation/>
       <Routes>
+        <Route path="/" element={<Navigate to='/login'/>} />
         <Route path="/register" element={<Register/>} />
+        <Route path="/login" element={<Login/>} />
       </Routes>
     </BrowserRouter>
   );
---
+++ client/src/components/navigation.jsx
@@ -0,0 +1,14 @@
+import {Link} from 'react-router-dom';
+
+
+
+export function Navigation() {
+    return(
+    <div>
+        <Link to ="/login">Login</Link>
+        <Link to="/register">Register</Link>
+    </div>
+    
+)
+
+}
