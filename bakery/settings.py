0d08a80f8ee664e983aca5382cc0211c4ae5ccc3
carlos <sandovalcarlos@uninorte.edu.co>
2024-07-16 01:23:54 -0500

    si

--- bakery/settings.py
+++ bakery/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'bakery_quilla',
         'USER': 'root',
-        'PASSWORD': 'sa1234',
+        'PASSWORD': '1234',
         'HOST': 'localhost',
         'PORT': '3306',
     }
